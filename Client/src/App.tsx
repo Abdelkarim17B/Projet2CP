0b225a8b2301e22be61b2fabb78a1dc8830047eb
Tarek Benameur <lt_benameur@esi.dz>
2023-04-16 20:20:32 +0100

    test

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,12 +2,15 @@ import Accueil from "./Pages/Accueil"
 import Populaire from "./Pages/Populaire"
 import SuggestionAttente from "./Pages/SuggestionAttente"
 import Suggestions from "./Pages/Suggestions"
+import ConditionUtilisation from './Pages/ConditionUtilisation';
+import Error404 from './Pages/Error404';
+import Apropos from './Pages/Apropos';
 
 function App() {
 
   return (
     <div className="bg-[#F0EFF2]">
-      <SuggestionAttente />
+      <Apropos/>
     </div>
   )
 }
