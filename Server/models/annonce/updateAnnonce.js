7cc7a38ec4cd1e2555f4ab3f52b990f59b88ebf2
Mehdi <lm_madani@esi.dz>
2023-04-27 15:28:40 +0100

    final client 2

--- Server/models/annonce/updateAnnonce.js
+++ Server/models/annonce/updateAnnonce.js
@@ -2,21 +2,13 @@ const { client } = require('pg');
 const { connectDB } = require('../connectDatabase');
 const { disconnectDB } = require('../disconnectDatabase');
 
-const fs = require('fs');
-const path = require('path');
-const imagePath = path.join(__dirname, 'haick.jpg');
-
 async function updateAnnonce(client, annonceObject) {
     try {
-        //await connectDB(client);
         await client.query(`UPDATE annonce SET id = '${annonceObject.id_annonce}', title = '${annonceObject.title}', subTitle = '${annonceObject.subTitle}', image = '${annonceObject.image}'`);
         console.log('annonce updated!');
     }
     catch (err) {
         console.error('Error updating annonce', err);
-    }
-    finally {
-        //await disconnectDB(client);
     }
 }
 
--- Server/models/prestation/deletePrestation.js
+++ Server/models/prestation/deletePrestation.js
@@ -1,7 +1,20 @@
 
+const { client } = require('pg')
+const { connectDB } = require('../connectDatabase');
+const { disconnectDB } = require('../disconnectDatabase');
 
-function deletePrestation() {
-    return null;
+
+
+async function deletePrestation(client, id_banque, categoriesList) {
+    try {
+        for (let category in categoriesList) {
+            await client.query(`DELETE FROM ${category} WHERE id_banque = ${id_banque}`);
+        }
+    }
+    catch (err) {
+        console.log('Error deleting process in prestation', err);
+    }
+
 }
 
-module.exports = {deletePrestation};
+module.exports = { deletePrestation };
