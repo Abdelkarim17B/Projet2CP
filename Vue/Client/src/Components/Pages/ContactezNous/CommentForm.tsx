8db5d8e05a36e244ba27ddd17f487223d47e7fa5
4zz0u4k <la_akeb@esi.dz>
2023-04-28 17:06:10 +0100

    well ...

---
+++ Vue/Client/src/Components/Pages/ContactezNous/CommentForm.tsx
@@ -0,0 +1,67 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+function CommentForm() {
+    function notify(){toast.success("Votre message a été bien envoyé ", {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      theme: "light",
+      });
+    }
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [sent , setSent] = useState(false);
+
+    function resetForm(){
+      setName("");
+      setEmail("");
+      setMessage("");
+      setTimeout(()=>{setSent(false),3000})
+    }
+
+    function formSubmit(e){
+      e.preventDefault();
+      let data = {
+        nom: name,
+        mail: email,
+        msg: message
+      }
+      axios.post("http://localhost:3000/send",data).then(res =>{
+        setSent(true);
+      }).then(notify).then(resetForm).catch(() => console.log("message not sent"))
+    }
+
+    return (
+      <form className="flex flex-col gap-[8vh]" method='POST'>
+          <div className="flex flex-col gap-[2vh]">
+            <input type="text" value={name} onChange={(e)=> {setName(e.target.value);console.log(name)}} className="bg-white max-w-[40vw] h-[10vh] font-bold flex items-center text-[1.3rem] rounded-[4px] text-Blue66 font-light pl-[5%]" name="Name" id="" placeholder="Name" />
+            <input type="e-mail" value={email} onChange={(e)=> {setEmail(e.target.value)}} className="bg-white max-w-[40vw] h-[10vh] font-bold flex items-center text-[1.3rem] rounded-[4px] text-Blue66 font-light pl-[5%]" name="E-mail" id="" placeholder="E-mail" />
+            <textarea value={message} onChange={(e)=> {setMessage(e.target.value)}} className="bg-white max-w-[40vw] pb-[20vh] font-bold flex pt-[5%] text-[1.3rem] rounded-[4px] text-Blue66 font-light pl-[5%]" name="Message" id="" placeholder="Message" />
+          </div>
+          <button className="bg-Red max-w-[40vw] h-[10vh] font-bold px-[15vw] text-[1.5rem] text-white rounded-[4px]" type="button" onClick={formSubmit}>Submit</button>
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+          />
+      </form>
+    );
+  };
+  
+  export default CommentForm;
