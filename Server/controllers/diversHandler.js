0ed6eb6bed6fb280ce1064bc19469c0fca92f1fc
Mehdi <lm_madani@esi.dz>
2023-05-05 08:14:40 +0100

    stats added

--- Server/controllers/diversHandler.js
+++ Server/controllers/diversHandler.js
@@ -1,6 +1,33 @@
 const express = require('express');
+const statsModel = require('../models/stat/getStat');
+const { connectDB } = require('../models/connection/connectDB');
+const { disconnectDB } = require('../models/disconnectDatabase');
 
 //handles '/suggestion/:id'
+
+const statsHandler = async (req,res) => {
+    const working_client = await connectDB('general');
+    try
+    {
+        const resultStats = await statsModel.getStat(working_client);
+        if (resultStats == null)
+        {
+            console.log('Error retrieveing the stats')
+        }
+        {
+            res.json(resultStats);
+        }
+    }
+    catch(err)
+    {
+        console.log("An Error occured in the server : " , err)
+    }
+    finally
+    {
+        disconnectDB(working_client);
+    }
+}
+
 const glossaryHandler = (req, res) => {
     res.send('this is the glossary');
 }
@@ -16,5 +43,6 @@ const proposHandler = (req,res) => {
 module.exports = {
     glossaryHandler,
     contactHandler,
-    proposHandler
+    proposHandler,
+    statsHandler
 };
